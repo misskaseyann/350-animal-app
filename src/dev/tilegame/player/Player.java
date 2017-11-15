d3f6f328c381a9bf8b83cd358b0311f204f00e39
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2017-11-14 19:53:41 -0500

    player updated

--- src/dev/tilegame/player/Player.java
+++ src/dev/tilegame/player/Player.java
@@ -1,11 +1,17 @@
 package dev.tilegame.player;
 
+import dev.tilegame.inventory.Inventory;
+
 /**
  * @author kaseystowell
  * @version 11.14.2017
  */
 public class Player {
 
+    Inventory inventory;
 
+    public Player(Inventory inventory) {
+
+    }
 
 }
