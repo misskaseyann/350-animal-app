4e4d98fbd0e167ef4c5611bb46f3c3df6391a540
Allison Bolen <bolena@mail.gvsu.edu>
2017-12-03 12:34:07 -0500

    load and save fixes

--- src/dev/tilegame/states/LoadState.java
+++ src/dev/tilegame/states/LoadState.java
@@ -30,6 +30,7 @@ public class LoadState extends State {
     StatsManager statsManager;
     GameState gs;
     Display dis;
+
     /**
      * MenuState constructor.
      *
@@ -54,89 +55,88 @@ public class LoadState extends State {
 
         String name = JOptionPane.showInputDialog("Enter a name for your file");
         //strip the spaces tho
-        name = name.replaceAll("\\s+", "");
-        if (name == "" || name == null /* or any amount of spaces*/) {
-            JOptionPane.showMessageDialog(null, "You need to include a name if you would like to save a game");
-            //buggy
-        }
-
-        try {
-            content = new Scanner(new File("../350-animal-app/savedData/" + name + ".txt")).useDelimiter("\\Z").next();
-            System.out.println(content);
-            //parse content
-            String[] info = content.split("\n");
-            int xspawn = Integer.parseInt(info[0].substring(info[0].indexOf(':') + 1));
-            int yspawn = Integer.parseInt(info[1].substring(info[1].indexOf(':') + 1));
-            String items = info[2].substring(info[2].indexOf(':') + 1);
-            String[] itemList = items.split(",");
-            int money = Integer.parseInt(info[3].substring(info[3].indexOf(':') + 1));
-            int health = Integer.parseInt(info[4].substring(info[4].indexOf(':') + 1));
-            int happy = Integer.parseInt(info[5].substring(info[5].indexOf(':') + 1));
-            int clean = Integer.parseInt(info[6].substring(info[6].indexOf(':') + 1));
-            int hunger = Integer.parseInt(info[7].substring(info[7].indexOf(':') + 1));
-            System.out.println(xspawn + " " + yspawn + " " + items + " " + money + " " + health + " " + happy + " " + clean + " " + hunger);
-
-            Inventory inven = new Inventory(manager);
-            //set xspawn and yspawn
-            // pet.setX(xspawn);
-            // pet.setY(yspawn);
-            //set items
-            for (String i : itemList) {
-                if (i.equals("Bacon Treat")) {
-                    BaconTreat bt = new BaconTreat();
-                    inven.addItem(bt);
-                }
-                else if (i.equals("Chew Toy")) {
-                    ChewToy ct = new ChewToy();
-                    inven.addItem(ct);
-                }
-                else if (i.equals("Dog Biscuit")) {
-                    DogBiscuit db = new DogBiscuit();
-                    inven.addItem(db);
-                }
-                else if (i.equals("Dog Bone")) {
-                    DogBone dbo = new DogBone();
-                    inven.addItem(dbo);
-                }
-                else if (i.equals("Dog Food Premium")) {
-                    DogFoodPremium dfp = new DogFoodPremium();
-                    inven.addItem(dfp);
-                }
-                else if (i.equals("Dog Food Standard")) {
-                    DogFoodStandard dfs = new DogFoodStandard();
-                    inven.addItem(dfs);
-                }
-                else if (i.equals("Stuffed Animal")) {
-                    StuffedAnimal sa = new StuffedAnimal();
-                    inven.addItem(sa);
-                }
-                else if (i.equals("Tennis Ball")) {
-                    TennisBall tb = new TennisBall();
-                    inven.addItem(tb);
-                }
+        if (name != null) {
+            name = name.replaceAll("\\s+", "");
+            if (name == "") {
+                JOptionPane.showMessageDialog(null, "You need to include a name if you would like to save a game");
+                State.setState(State.getLastState());
             }
-            //set inventory
-            player.setInventory(inven);
-            //set money
-            player.setMoney(money);
-            //set health
-            pet.setHealth(health);
-            //set happy
-            pet.setHappiness(happy);
-            //set clean
-            pet.setCleanliness(clean);
-            // set hunger
-            pet.setHunger(hunger);
 
-            manager.setPet(pet);
-            manager.setPlayer(player);
+            try {
+                content = new Scanner(new File("../350-animal-app/savedData/" + name + ".txt")).useDelimiter("\\Z").next();
+                System.out.println(content);
+                //parse content
+                String[] info = content.split("\n");
+                int xspawn = Integer.parseInt(info[0].substring(info[0].indexOf(':') + 1));
+                int yspawn = Integer.parseInt(info[1].substring(info[1].indexOf(':') + 1));
+                String items = info[2].substring(info[2].indexOf(':') + 1);
+                String[] itemList = items.split(",");
+                int money = Integer.parseInt(info[3].substring(info[3].indexOf(':') + 1));
+                int health = Integer.parseInt(info[4].substring(info[4].indexOf(':') + 1));
+                int happy = Integer.parseInt(info[5].substring(info[5].indexOf(':') + 1));
+                int clean = Integer.parseInt(info[6].substring(info[6].indexOf(':') + 1));
+                int hunger = Integer.parseInt(info[7].substring(info[7].indexOf(':') + 1));
+                System.out.println(xspawn + " " + yspawn + " " + items + " " + money + " " + health + " " + happy + " " + clean + " " + hunger);
 
-            //set game state == new
-            State.setState(manager.getGame().getGameState());
-        } catch (FileNotFoundException e) {
-            JOptionPane.showMessageDialog(null, "<html>The file <b>"+name+"</b> is non-existent. \n The directory \"/350-animal-app/savedData/\" is where the files are saved. \n Please make sure your spelled your file name correctly and that it is in the right directory.");
-            System.out.println(State.getLastState());
+                Inventory inven = new Inventory(manager);
+                //set xspawn and yspawn
+                // pet.setX(xspawn);
+                // pet.setY(yspawn);
+                //set items
+                for (String i : itemList) {
+                    if (i.equals("Bacon Treat")) {
+                        BaconTreat bt = new BaconTreat();
+                        inven.addItem(bt);
+                    } else if (i.equals("Chew Toy")) {
+                        ChewToy ct = new ChewToy();
+                        inven.addItem(ct);
+                    } else if (i.equals("Dog Biscuit")) {
+                        DogBiscuit db = new DogBiscuit();
+                        inven.addItem(db);
+                    } else if (i.equals("Dog Bone")) {
+                        DogBone dbo = new DogBone();
+                        inven.addItem(dbo);
+                    } else if (i.equals("Dog Food Premium")) {
+                        DogFoodPremium dfp = new DogFoodPremium();
+                        inven.addItem(dfp);
+                    } else if (i.equals("Dog Food Standard")) {
+                        DogFoodStandard dfs = new DogFoodStandard();
+                        inven.addItem(dfs);
+                    } else if (i.equals("Stuffed Animal")) {
+                        StuffedAnimal sa = new StuffedAnimal();
+                        inven.addItem(sa);
+                    } else if (i.equals("Tennis Ball")) {
+                        TennisBall tb = new TennisBall();
+                        inven.addItem(tb);
+                    }
+                }
+                //set inventory
+                player.setInventory(inven);
+                //set money
+                player.setMoney(money);
+                //set health
+                pet.setHealth(health);
+                //set happy
+                pet.setHappiness(happy);
+                //set clean
+                pet.setCleanliness(clean);
+                // set hunger
+                pet.setHunger(hunger);
+
+                manager.setPet(pet);
+                manager.setPlayer(player);
+
+                //set game state == new
+                State.setState(manager.getGame().getGameState());
+            } catch (FileNotFoundException e) {
+                JOptionPane.showMessageDialog(null, "<html>The file <b>\"" + name + "\"</b> is non-existent. \n The directory \"/350-animal-app/savedData/\" is where the files are saved. \n Please make sure your spelled your file name correctly and that it is in the right directory.");
+                System.out.println(State.getLastState());
+                State.setState(State.getLastState());
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "No file loaded.");
             State.setState(State.getLastState());
+
         }
 
 
--- src/dev/tilegame/states/SaveState.java
+++ src/dev/tilegame/states/SaveState.java
@@ -17,8 +17,8 @@ import dev.tilegame.entities.animals.Pet;
 
 public class SaveState extends State {
 
-    Game game ;
-    Pet pet ;
+    Game game;
+    Pet pet;
     Player player;
 
     /**
@@ -39,42 +39,51 @@ public class SaveState extends State {
         int x = manager.getMouseManager().getMouseX();
         int y = manager.getMouseManager().getMouseY();
         String itemlist = "";
-        if(player.getInventory().getItemList().size() > 0) {
+        if (player.getInventory().getItemList().size() > 0) {
             for (Item item : player.getInventory().getItemList()) {
                 System.out.println(item.getName());
                 itemlist += item.getName() + ",";
             }
+        } else {
+            itemlist = "NA";
         }
-        else {itemlist = "NA";}
         //String name = "hellow is it me youre looking for ";
-        String name = JOptionPane.showInputDialog("Enter a name for your file");
-        //strip the spaces tho
-        name = name.replaceAll("\\s+", "");
-        if (name == "" || name == null /* or any amount of spaces*/) {
-            JOptionPane.showMessageDialog(null, "You need to include a name if you would like to save a game");
-            //buggy
+        String name = JOptionPane.showInputDialog("Enter a name for your file.");
+
+        if (name != null) {
+            name = name.replaceAll("\\s+", "");
+            if (name.equals("")) {
+                JOptionPane.showMessageDialog(null, "You need to include a name if you would like to save a game");
+                State.setState(State.getLastState());
+            } else {
+                File file = new File("../350-animal-app/savedData/" + name + ".txt");
+                try {
+                    PrintWriter writer = new PrintWriter("../350-animal-app/savedData/" + name + ".txt");
+
+                    //writer.println("State: " + State.getLastState()); will always load to game state with the following data
+                    writer.println("Xloc:" + State.getCurrentState().manager.getPet().gridLocX());
+                    writer.println("Yloc:" + State.getCurrentState().manager.getPet().gridLocY());
+                    writer.println("Items:" + itemlist);
+                    writer.println("Money:" + player.getMoney());
+                    writer.println("Health:" + pet.getHealth());
+                    writer.println("Happiness:" + pet.getHappiness());
+                    writer.println("Hygene:" + pet.getCleanliness());
+                    writer.println("Hunger:" + pet.getHunger());
+
+                    writer.close();
+
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
+
+                JOptionPane.showMessageDialog(null, "<html>File <b>" + name + "</b> saved your current game! </html>");
+
+                State.setState(State.getLastState());
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "The file did not save. Please try again.");
+            State.setState(State.getLastState());
         }
-        File file = new File("../350-animal-app/savedData/" + name + ".txt");
-        try {
-            PrintWriter writer = new PrintWriter("../350-animal-app/savedData/" + name + ".txt");
-
-            //writer.println("State: " + State.getLastState()); will always load to game state with the following data
-            writer.println("Xloc:" + State.getCurrentState().manager.getPet().gridLocX());
-            writer.println("Yloc:" + State.getCurrentState().manager.getPet().gridLocY());
-            writer.println("Items:" + itemlist);
-            writer.println("Money:" + player.getMoney());
-            writer.println("Health:" + pet.getHealth());
-            writer.println("Happiness:" + pet.getHappiness());
-            writer.println("Hygene:" + pet.getCleanliness());
-            writer.println("Hunger:" + pet.getHunger());
-
-            writer.close();
-
-        } catch (IOException ie) {
-            ie.printStackTrace();
-        }
-        JOptionPane.showMessageDialog(null, "<html>File <b>" + name + "</b> saved your current game! </html>");
-        State.setState(State.getLastState());
     }
 
     @Override
