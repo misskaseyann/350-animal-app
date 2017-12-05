2e81d790405052e12ace1f4e9fa368d9c63c5979
Brendan Nahed <brendannahed@Brendans-MacBook-Pro.local>
2017-12-05 01:16:31 -0500

    StorebuyState checkstyle

--- src/dev/tilegame/states/StoreBuyState.java
+++ src/dev/tilegame/states/StoreBuyState.java
@@ -14,19 +14,21 @@ import java.awt.*;
  * @version 11.26.2017
  */
 public class StoreBuyState extends State {
+    /**item is the Item a user can buy.*/
     private Item item;
+    /**player holds the user's Player.*/
     private Player player;
 
     /**
      * Basic constructor.
      *
-     * @param manager game manager.
-     * @param item item to be bought.
+     * @param inManager game manager.
+     * @param inItem item to be bought.
      */
-    public StoreBuyState(Manager manager, Item item) {
-        super(manager);
-        this.item = item;
-        this.player = manager.getPlayer();
+    public StoreBuyState(final Manager inManager, final Item inItem) {
+        super(inManager);
+        this.item = inItem;
+        this.player = inManager.getPlayer();
     }
 
     /**
@@ -41,7 +43,8 @@ public class StoreBuyState extends State {
             int y = manager.getMouseManager().getMouseY();
             // Buy
             if ((x > 188 && x < 344) && (y > 583 && y < 622)) {
-                if ((player.getMoney() >= item.getCost()) && (player.getInventory().getSize() < 12)) {
+                if ((player.getMoney() >= item.getCost())
+                        && (player.getInventory().getSize() < 12)) {
                     player.setMoney(player.getMoney() - item.getCost());
                     player.getInventory().addItem(item);
                     System.out.println("Added " + item.getName());
@@ -61,22 +64,27 @@ public class StoreBuyState extends State {
      * @param g graphics object.
      */
     @Override
-    public void render(Graphics g) {
+    public void render(final Graphics g) {
         FontMetrics fm = g.getFontMetrics();
-        if ((player.getMoney() < item.getCost()) || (player.getInventory().getSize() > 11))
+        if ((player.getMoney() < item.getCost())
+                || (player.getInventory().getSize() > 11)) {
             g.drawImage(Assets.getShopCancel(), 0, 0, null);
-        else
+        } else {
             g.drawImage(Assets.getShopBuy(), 0, 0, null);
+        }
         g.drawImage(item.getImg(), 148, 376, null);
         g.setFont(manager.getFont());
         String money = "Your Money: $" + Integer.toString(player.getMoney());
         g.drawString(money, (328 - fm.stringWidth(money)) / 2, 343);
-        g.drawString(item.getName(), (328 - fm.stringWidth(item.getName())) / 2, 458);
+        g.drawString(item.getName(),
+                (328 - fm.stringWidth(item.getName())) / 2, 458);
         String cost = "Price: $" + Integer.toString(item.getCost());
         int y = 468 + fm.getAscent();
         g.drawString(cost, (328 - fm.stringWidth(cost)) / 2, y);
-        for (String line : item.getDescription().split("\n"))
-            g.drawString(line, ((270 - fm.stringWidth(line)) / 2), y += g.getFontMetrics().getHeight());
+        for (String line : item.getDescription().split("\n")) {
+            g.drawString(line, ((270 - fm.stringWidth(line)) / 2), y
+                    += g.getFontMetrics().getHeight());
+        }
     }
 
     /**
