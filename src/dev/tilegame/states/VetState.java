297a5ffa77afac188fd6543e7c896239f6edabc5
Brendan Nahed <brendannahed@Brendans-MacBook-Pro.local>
2017-12-05 01:22:50 -0500

    vetState CheckStyle

--- src/dev/tilegame/states/VetState.java
+++ src/dev/tilegame/states/VetState.java
@@ -3,7 +3,6 @@ package dev.tilegame.states;
 import dev.tilegame.Manager;
 import dev.tilegame.gfx.Assets;
 import dev.tilegame.player.Player;
-import dev.tilegame.sound.LoopLoader;
 import dev.tilegame.worlds.World;
 
 import java.awt.*;
@@ -13,15 +12,15 @@ import java.awt.*;
  * @version 12.02.2017
  */
 public class VetState extends State {
+    /**player holds the user's Player.*/
     private Player player;
-    private LoopLoader music;
 
     /**
      * StoreState constructor.
      *
      * @param manager game class manager.
      */
-    public VetState(Manager manager) {
+    public VetState(final Manager manager) {
         super(manager);
         this.player = manager.getPlayer();
     }
@@ -55,24 +54,36 @@ public class VetState extends State {
         }
     }
 
+    /**
+     * render creates the graphics for the VetState.
+     * @param g graphics object.
+     */
     @Override
-    public void render(Graphics g) {
+    public void render(final Graphics g) {
         FontMetrics fm = g.getFontMetrics();
-        if ((player.getMoney() < 50))
+        if ((player.getMoney() < 50)) {
             g.drawImage(Assets.getVetCancel(), 0, 0, null);
-        else
+        } else {
             g.drawImage(Assets.getVetMenu(), 0, 0, null);
+        }
         g.setFont(manager.getFont());
         String money = "Your Money: $" + Integer.toString(player.getMoney());
         g.drawString(money, (328 - fm.stringWidth(money)) / 2, 500);
         String cost = "Vet Visit Cost: $50";
         g.drawString(cost, (315 - fm.stringWidth(cost)) / 2, 525);
-        String description = "The vet will heal your pet completely.\nAll hearts will be maxed out!";
+        String description = "The vet will heal your pet completely."
+                + "\nAll hearts will be maxed out!";
         int y = 515 + fm.getAscent();
-        for (String line : description.split("\n"))
-            g.drawString(line, ((265 - fm.stringWidth(line)) / 2), y += g.getFontMetrics().getHeight());
+        for (String line : description.split("\n")) {
+            g.drawString(line, ((265 - fm.stringWidth(line)) / 2),
+                    y += g.getFontMetrics().getHeight());
+        }
     }
 
+    /**
+     * No world to return.
+     * @return null.
+     */
     @Override
     public World getWorld() {
         return null;
